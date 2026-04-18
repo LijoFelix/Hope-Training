233c91f8839058b3b79118e06c5016d0ed2974cd
LijoFelix <lijofelix207@gmail.com>
2026-04-18 13:50:14 +0530

    Day 21

--- Day_20/Synchronizeexample.java
+++ Day_20/Synchronizeexample.java
@@ -1,38 +1,31 @@
 class Bank{
-    int bal = 1000;
+    int bal=1000;
     synchronized void withdraw(int amount){
-        if(amount <= bal){
+        if(amount<=bal){
             System.out.println("Amount withdraw successfully");
-            bal -= amount;
+            bal-=amount;
             System.out.println("Remaining balance ="+ bal);
         }
         else{
             System.out.println("Insufficient balance");
         }
-
     }
 }
 class bankthread extends Thread{
     Bank bank;
-
     public bankthread(Bank bank) {
-        this.bank = bank;
+        this.bank=bank;
     }
     public void run(){
         bank.withdraw(800);
     }
-    
 }
 public class Synchronizeexample {
     public static void main(String[] args) {
-        
-    
         Bank b = new Bank();
         bankthread b1 = new bankthread(b);
         bankthread b2 = new bankthread(b);
         b1.start();
         b2.start();
     }
-
-
 }
